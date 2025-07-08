7c922718f3261731340ddc483899cfa1f8d408b5
Shelby Norris <shelbs9109@gmail.com>
2025-07-07 19:42:21 -0500

    created fetch character function, began render character function that creates display cards for each character with image, name, and description from the API, began pagination logic, created loading gif function that adds a loading gif in the chaacter container

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-class Character {
-    constructor(name, image) {
-        this.name = name;
-        this.image = image;
-    }
-    renderCard() {
-        
-    }
-}
+// class Character {
+//     constructor(name, image) {
+//         this.name = name;
+//         this.image = image;
+//     }
+//     renderCard() {
 
+//     }
+// }
+
+const state = {
+  currentPage: 1,
+  currentName: "",
+  characterData: [],
+};
 
 // Retrieve API data
-async function fetchCharacters() {
+async function fetchCharacters(page) {
   try {
-    const response = await fetch(`https://starwars-databank-server.vercel.app/api/v1/characters`);
+    const response = await fetch(
+      `https://starwars-databank-server.vercel.app/api/v1/characters?_page=${page}&limit=10`
+    );
     if (!response.ok) {
       throw new Error("Network Error ", response.status);
     }
@@ -28,3 +35,50 @@ async function fetchCharacters() {
 }
 
 fetchCharacters();
+
+// Display characters on page
+function renderCharacters(characters) {
+  const characterContainer = document.getElementById("character-container");
+  characterContainer.innerHTML = "";
+  // characterContainer.className =
+
+  characters.forEach((character) => {
+    const characterElement = document.createElement("div");
+    // characterElement.className =
+
+    const imageElement = document.createElement("img");
+    imageElement.src = character.image;
+    imageElement.alt = character.name;
+    // imageElement.className = 
+
+    const nameElement = document.createElement("h2");
+    nameElement.innerHTML = character.name?.[0] || "Character not found";
+    // nameElement.className =
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = character.description;
+
+    characterElement.appendChild(imageElement);
+    characterElement.appendChild(nameElement);
+    characterElement.appendChild(descriptionElement);
+
+    characterContainer.appendChild(characterElement);
+  });
+}
+
+// Pagination Logic
+function renderPagination(numFound) {
+  const totalPages = Math.ceil(numFound / 10);
+
+  const paginationContainer = document.getElementById("pagination-container");
+  paginationContainer.innerHTML = "";
+
+  const prevBtn = document.createElement("button");
+  prevBtn.textContent = "Previous";
+  // prevBtn.className = 
+  prevBtn.disabled = state.currentPage === 1;
+  prevBtn.onclick = async () => {
+    state.currentPage--; // decrease the page number
+    const data = await 
+  }
+}
